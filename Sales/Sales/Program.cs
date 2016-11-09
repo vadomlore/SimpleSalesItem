60ab30d8d10b1b33580631c3c17b00e137992c2e
zhangyile <zhangyile1161@virtuos.com.cn>
2016-11-09 14:04:33 +0800

    add sales

---
+++ Sales/Sales/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Sales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var items = new List<SalesItem>(){
+                new SalesItem("xxxx-1", 10),
+                new SalesItem("xxxx-1", 10),
+                new SalesItem("xxxx-3", 12),
+                new SalesItem("xxxx-3", 13),
+                new SalesItem("xxxx-4", 15),
+            };
+            var salesManager = new SalesManager();
+            items.ForEach(item => salesManager.Add(item));
+            salesManager.SetCategory("xxxx-1", DiscountCategory.C);
+            Console.WriteLine(salesManager.SumPrice("xxxx-1"));
+        }
+    }
+}
---
+++ Sales/Sales/SalesItems.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sales
+{
+    public enum DiscountCategory
+    {
+        A,
+        B,
+        C
+    }
+
+
+    public class SalesItem
+    {
+        public string ISBN { get; set; }
+        public double Price {
+            get;set;
+        }
+
+        public SalesItem(string isbn, double price)
+        {
+            this.ISBN = isbn;
+            this.Price = price;
+        }
+    }
+
+    public class SalesManager
+    {
+        List<SalesItem> salesItems = new List<SalesItem>();
+        private Dictionary<string, DiscountCategory> discountCategories = new Dictionary<string, DiscountCategory>();
+
+        public void Add(SalesItem salesItem)
+        {
+            salesItems.Add(salesItem);
+        }
+
+        /// <summary>
+        /// set category for same isbn product
+        /// </summary>
+        /// <param name="isbn"></param>
+        /// <param name="category"></param>
+        public void SetCategory(string isbn, DiscountCategory category)
+        {
+            discountCategories[isbn] = category;
+        }
+
+        public double SumPrice(string isbn)
+        {
+            var price = salesItems.Where(i => i.ISBN == isbn).Sum(x => { return x.Price; });
+            return discountCategories.ContainsKey(isbn) ? price * DiscountCategoryToRate(discountCategories[isbn]): price;
+        }
+
+        public static double DiscountCategoryToRate(DiscountCategory category)
+        {
+            switch (Enum.GetName(typeof(DiscountCategory), category))
+            {
+                case "A": return 1;
+                case "B": return 0.9;
+                case "C": return 0.7;
+                default: return 1;
+            }
+        }
+
+    }
+}
